34319d8381e67e101d045bd9fabcce509b602679
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2024-01-30 12:41:14 +0100

    Add files via upload

---
+++ zadatak2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sortiranjestringova
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Unesite prvu rijec:");
+
+            string prviString = Console.ReadLine();
+
+            Console.WriteLine("Unesite drugu rijec:");
+
+            string drugiString = Console.ReadLine();
+
+            string[] stringovi = { prviString, drugiString };
+
+            Array.Sort(stringovi);
+
+            Console.WriteLine("Sortirani rijeci:");
+
+            foreach (string s in stringovi)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
