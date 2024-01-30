b228dc7242f2b2bdd68c137e62d8e11fee36aa11
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2024-01-30 12:34:38 +0100

    Add files via upload

---
+++ zadatak6.cs
@@ -0,0 +1,25 @@
+﻿public class Neboder
+
+{ 
+
+    private double visina;
+
+    private int brojkatova;
+
+    public double Visina
+
+    {
+        get { return visina; }
+
+        set { visina = value; }
+
+    }
+
+    public int Brojkatova
+
+    {
+        get { return brojkatova; }
+        set { brojkatova = value; }
+
+    }
+}
---
+++ zadatak7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+public class KlasaX
+{
+
+    private int broj;
+
+
+    public int Broj
+    {
+
+        get { return broj; }
+        set { broj = value; }
+    }
+
+}
+
+public class KlasaY : KlasaX
+
+{
+
+}
+
+public class Program
+
+{
+    public static void Main(string[] args)
+    {
+
+        KlasaY y = new KlasaY();
+
+        y.Broj = 10;
+
+        Console.WriteLine("Vrijednost varijable broj: " + y.Broj);
+    }
+}
+
